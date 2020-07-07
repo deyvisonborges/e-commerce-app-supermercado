d7c8fa03ef083616a3a826225574b49aa0b3647b
Deyvison Borges <deyvisonsilvaborges@gmail.com>
2020-07-07 11:56:38 -0300

    feature/elements => criando pattern pra buscar imagens estáticas

---
+++ source/utils/icones.js
@@ -0,0 +1,8 @@
+export const requires = {
+  carnes: require("../assets/icone-carne.png"),
+  enlatados: require("../assets/icone-frios.png"),
+  hortfruit: require('../assets/icone-hortfruit.png'),
+  massas: require("../assets/icone-massas.png"),
+  peixaria: require("../assets/icone-frios.png"),
+  salgados: require("../assets/icone-lanche.png")
+}
